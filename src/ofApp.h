917df8b1bbd77a6d994e330f9309344d7a8596a2
mcthomas <matt.thomas2@icloud.com>
2023-09-24 22:55:19 -0500

    Init src

---
+++ src/ofApp.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ofxiOS.h"
+
+class ofApp : public ofxiOSApp{
+	
+	public:
+		void setup();
+		void update();
+		void draw();	
+};
+
+
---
+++ src/ofApp.mm
@@ -0,0 +1,20 @@
+#include "ofApp.h"
+
+ofBoxPrimitive box;
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	box.set( 300 );
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	box.setPosition(ofGetWidth() / 2, ofGetHeight() / 2, 0);
+	box.rotate(0.7, 0, 1.0, 0.0);
+	box.drawWireframe();
+}
